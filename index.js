5122c5b851cfd6986d08852d4557b30e889dbc5c
andreocunha <andreoliveiracunha20@gmail.com>
2020-12-18 18:36:34 -0300

    websocket-server

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 // server.js
+const app = require('express')()
+const server = require('http').createServer(app)
 const WebSocket = require('ws')
+var port = process.env.PORT || 4000;
 
-const wss = new WebSocket.Server({ port: 4000 })
+
+const wss = new WebSocket.Server({ server })
 
 let temperatura = 0
 
@@ -15,3 +19,8 @@ wss.on('connection', ws => {
     setInterval(function () { temperatura+=1; ws.send(temperatura); }, 1000);
 
 })
+
+server.listen(port, function () {
+    console.log('listening on port 4000')
+})
+  
