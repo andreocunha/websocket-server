8cb180f78ae14ba2afd242c7902cc7bca25a1d11
andreocunha <andreoliveiracunha20@gmail.com>
2020-12-18 16:52:17 -0300

    websocket-server

---
+++ index.js
@@ -0,0 +1,18 @@
+// server.js
+
+const WebSocket = require('ws')
+
+const wss = new WebSocket.Server({ port: 4000 })
+
+let temperatura = '0'
+
+wss.on('connection', ws => {
+    console.log('Nova conexão')
+
+    ws.on('message', message => {
+        temperatura = message
+    })
+
+    setInterval(function () { ws.send(temperatura); }, 1000);
+
+})
